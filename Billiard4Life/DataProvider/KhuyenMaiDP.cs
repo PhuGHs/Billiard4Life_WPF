62f351837ce280685a016f8f145ad9361d8cce2e
Phu Le <21522466@gm.uit.edu.vn>
2023-03-21 11:16:15 +0700

    finish khuyenmai

--- Billiard4Life/DataProvider/KhuyenMaiDP.cs
+++ Billiard4Life/DataProvider/KhuyenMaiDP.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,15 +38,20 @@ namespace Billiard4Life.DataProvider
                     string? tenkm = Convert.ToString(dr["TenKM"]);
                     int giamgia = Convert.ToInt32(dr["GiamGia"]);
                     Decimal mucapdung = Convert.ToDecimal(dr["MucApDung"]);
-                    DateTime ngaybd = Convert.ToDateTime(dr["BatDau"]);
-                    DateTime ngaykt = Convert.ToDateTime(dr["KetThuc"]);
+                    string trangthai = Convert.ToString(dr["TrangThai"]);
+                    string ngaybd = Convert.ToDateTime(dr["BatDau"]).ToShortDateString();
+                    string ngaykt = Convert.ToDateTime(dr["KetThuc"]).ToShortDateString();
                     string? mota = Convert.ToString(dr["MoTa"]);
 
-                    if (makm == null || tenkm == null || mota == null)
+                    if (makm == null 
+                        || tenkm == null 
+                        || mota == null 
+                        || ngaybd == null 
+                        || ngaykt == null)
                     {
                         throw new ArgumentNullException("Thieu property khong the lay len tu database");
                     }
-                    KhuyenMais.Add(new KhuyenMai(makm, tenkm, giamgia, mucapdung, DateOnly.FromDateTime(ngaybd), DateOnly.FromDateTime(ngaykt), mota));
+                    KhuyenMais.Add(new KhuyenMai(makm, tenkm, mucapdung, trangthai, ngaybd, ngaykt, mota, giamgia));
                 }
             }
             catch (Exception ex)
@@ -58,6 +64,67 @@ namespace Billiard4Life.DataProvider
                 DBClose();
             }
             return KhuyenMais;
+        }
+        public void AddKhuyenMai(KhuyenMai item)
+        {
+            try
+            {
+                DBOpen();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO KHUYENMAI VALUES (@makm, @tenkm, @giam, @mucapdung, @ngaybd, @ngaykt, @mota, @trangthai)";
+                cmd.Parameters.AddWithValue("@makm", item.MAKM);
+                cmd.Parameters.AddWithValue("@tenkm", item.TenKM);
+                cmd.Parameters.AddWithValue("@giam", item.GiamGia);
+                cmd.Parameters.AddWithValue("@mucapdung", item.MucApDung);
+                cmd.Parameters.AddWithValue("@ngaybd", item.NgayBatDau);
+                cmd.Parameters.AddWithValue("@ngaykt", item.NGayKetThuc);
+                cmd.Parameters.AddWithValue("@mota", item.MoTa);
+                cmd.Parameters.AddWithValue("@trangthai", item.TrangThai);
+
+                cmd.Connection = SqlCon;
+
+                cmd.ExecuteNonQuery();
+            } finally
+            {
+                DBClose();
+            }
+        }
+        public void DeleteKhuyenmai(string makm)
+        {
+            try
+            {
+                DBOpen();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "DELETE FROM KHUYENMAI WHERE MAKM = @makm";
+                cmd.Parameters.AddWithValue("@makm", makm);
+                cmd.Connection=SqlCon;
+
+                cmd.ExecuteNonQuery();
+            }
+            finally { DBClose(); }
+        }
+        public void UpdateKhuyenMai(KhuyenMai item)
+        {
+            try
+            {
+                DBOpen();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "UPDATE KHUYENMAI SET TenKM = @tenkm, BatDau = @ngaybd, KetThuc = @ngaykt, GiamGia = @giam,  MucApDung = @mucapdung, MoTa = @mota, TrangThai = @trangthai WHERE MaKM = @makm";
+                cmd.Parameters.AddWithValue("@tenkm", item.TenKM);
+                cmd.Parameters.AddWithValue("@ngaybd", item.NgayBatDau);
+                cmd.Parameters.AddWithValue("@ngaykt", item.NGayKetThuc);
+                cmd.Parameters.AddWithValue("@giam", item.GiamGia);
+                cmd.Parameters.AddWithValue("@mucapdung", item.MucApDung);
+                cmd.Parameters.AddWithValue("@mota", item.MoTa);
+                cmd.Parameters.AddWithValue("@trangthai", item.TrangThai);
+                cmd.Parameters.AddWithValue("@makm", item.MAKM);
+                cmd.Connection = SqlCon;
+
+                cmd.ExecuteNonQuery();
+            } finally
+            {
+                DBClose();
+            }
         }
     }
 }
--- Billiard4Life/Models/KhuyenMai.cs
+++ Billiard4Life/Models/KhuyenMai.cs
@@ -14,9 +14,11 @@ namespace Billiard4Life.Models
         private string _TenKM;
         private int _GiamGia;
         private Decimal _MucApDung;
-        private DateOnly _NgayBatDau;
-        private DateOnly _NgayKetThuc;
+        private string _NgayBatDau;
+        private string _NgayKetThuc;
         private string _MoTa;
+        private bool _isSelected;
+        private string _trangThai;
         #endregion
 
         #region properties
@@ -24,12 +26,14 @@ namespace Billiard4Life.Models
         public string TenKM { get { return _TenKM; } set { _TenKM = value; OnPropertyChanged(); } }
         public int GiamGia { get { return _GiamGia; } set { _GiamGia = value; OnPropertyChanged(); } }
         public Decimal MucApDung { get { return _MucApDung; } set { _MucApDung = value; OnPropertyChanged(); } }
-        public DateOnly NgayBatDau { get { return _NgayBatDau; } set { _NgayBatDau = value; OnPropertyChanged(); } }
-        public DateOnly NGayKetThuc { get { return _NgayKetThuc; } set { _NgayKetThuc = value; OnPropertyChanged(); } }
+        public string NgayBatDau { get { return _NgayBatDau; } set { _NgayBatDau = value; OnPropertyChanged(); } }
+        public string NGayKetThuc { get { return _NgayKetThuc; } set { _NgayKetThuc = value; OnPropertyChanged(); } }
         public string MoTa { get { return _MoTa; } set { _MoTa = value; OnPropertyChanged(); } }
+        public string TrangThai { get { return _trangThai; } set { _trangThai = value; OnPropertyChanged(); } }
+        public bool IsSelected { get { return _isSelected; } set { _isSelected = value; OnPropertyChanged();} }
         #endregion
 
-        public KhuyenMai(string makm, string tenkm, int giamgia, Decimal mucgiam, DateOnly ngaybd, DateOnly ngayketthuc, string mota)
+        public KhuyenMai(string makm, string tenkm, Decimal mucgiam, string trangthai, string ngaybd, string ngayketthuc, string mota, int giamgia)
         {
             _MAKM = makm;
             _TenKM = tenkm;
@@ -37,7 +41,47 @@ namespace Billiard4Life.Models
             _MucApDung = mucgiam;
             _NgayBatDau = ngaybd;
             _NgayKetThuc = ngayketthuc;
+            _trangThai = trangthai;
             _MoTa = mota;
+            _isSelected = false;
+        }
+
+        public KhuyenMai()
+        {
+            _GiamGia = 0;
+            _MAKM = String.Empty;
+            _TenKM = String.Empty;
+            _trangThai = String.Empty;
+            _MoTa = String.Empty;
+            _NgayBatDau= String.Empty;
+            _NgayKetThuc = String.Empty;
+            _MucApDung = 0;
+        }
+
+        public bool isNullOrEmpty()
+        {
+            if(string.IsNullOrEmpty(_MAKM) 
+                || string.IsNullOrEmpty(_TenKM) 
+                || string.IsNullOrEmpty(_MoTa) 
+                || string.IsNullOrEmpty(_trangThai) 
+                || string.IsNullOrEmpty(_NgayBatDau) 
+                || string.IsNullOrEmpty(_NgayKetThuc) 
+                || _GiamGia == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void clear()
+        {
+            _GiamGia = 0;
+            _MAKM = String.Empty;
+            _TenKM = String.Empty;
+            _trangThai = String.Empty;
+            _MoTa = String.Empty;
+            _NgayBatDau = String.Empty;
+            _NgayKetThuc = String.Empty;
+            _MucApDung = 0;
         }
     }
 }
